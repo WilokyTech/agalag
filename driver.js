f680a94b0bc43ad3c708a06ba340a8eef1c906a1
Kytech <Kytech@users.noreply.github.com>
2023-04-12 01:09:20 -0600

    Setup upscaling to occur only at integer multiples.

--- driver.js
+++ driver.js
@@ -4,6 +4,29 @@ import { InputManager } from "./InputManager.js";
 import { ParticleSystem } from "./particleSystem.js";
 import { Renderer } from "./renderer.js";
 
+// Adjust canvas display size to be a whole integer multiple when upscaling.
+// In combination with the CSS on the canvas, this ensures that we get nice sharp pixels
+// when we upscale. For downscaling, we will probably want to compare the different
+// downsampling methods on final assets. Downsampling cases should be easiest using CSS
+// media queries if we need to only permit downscaling by powers of two since we don't
+// want to downsample to smaller than 16x16 tile size.
+function scaleCanvas() {
+    const gameCanvas = document.getElementById('canvas');
+    const gameHeight = Number.parseInt(gameCanvas.getAttribute('height'));
+    
+    if (window.innerHeight > gameHeight) {
+        const gameWidth = Number.parseInt(gameCanvas.getAttribute('width'));
+        const sizeScaleFactor = Math.floor(window.innerHeight / gameHeight);
+        gameCanvas.style.width = `${gameWidth * sizeScaleFactor}px`;
+        gameCanvas.style.height = `${gameHeight * sizeScaleFactor}px`;
+    } else {
+        gameCanvas.style.height = '';
+        gameCanvas.style.width = '';
+    }
+}
+document.addEventListener("DOMContentLoaded", scaleCanvas);
+window.addEventListener("resize", scaleCanvas);
+
 let prevTime = performance.now();
 let elapsedTime;
 let timeRan = 0;
