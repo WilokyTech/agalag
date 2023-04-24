f0aa66c6f419096344aae7f8b0c63851246fd1c5
WizardRyan <ryan.andersen117@gmail.com>
2023-04-23 18:05:09 -0600

    add woosh and laser sfx, minor change on server to comply with Dean's request to log port num

--- server.js
+++ server.js
@@ -5,5 +5,6 @@ const open = require('open');
 app.use(express.static('./'));
 
 const server = app.listen(5050, function () {
+   console.log(`Server listening on port 5050`);
    open(`http://localhost:${5050}`);
 });
