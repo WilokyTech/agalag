f1693a255bc039677775eb0e8ef719aea8175aad
Kytech <Kytech@users.noreply.github.com>
2023-04-25 16:10:10 -0600

    Start extracting formation out of enemy manager.

--- enemyManager.js
+++ enemyManager.js
@@ -28,6 +28,146 @@ const ATTACK_RUN_INTERVAL = 5000;
 const ATTACK_GAP = 1000;
 const ATTACK_RUN_ENEMY_CT = 3;
 
+class EnemyFormation {
+  constructor() {
+    /** @type {Array<Vector2>} */
+    this.baseFormationPositions = [];
+    /** @type {Array<Vector2>} */
+    this.destFormationPositions = [];
+    /**_@type {Array<Vector2>} */
+    this.spreadFormationPositions = [];
+    
+    this.elapsedMovementTime = 0;
+    this.formationMovementTime = FORMATION_MOVEMENT_TIME;
+    this.cyclesUntilFormationSwitch = Infinity;
+    
+    this.#computeLocations();
+  }
+  
+  #computeLocations() {
+    const gameWidthInSprites = Math.floor(GameManager.canvas.width / ENEMY_SPRITE_SIZE);
+    const gapCount = enemyLayout[0].length - 1;
+    const openSpace = (gameWidthInSprites - enemyLayout[0].length) * ENEMY_SPRITE_SIZE;
+    const gapSize = Math.floor(openSpace / gapCount);
+    const spreadPositionsStartX = (openSpace - gapSize * gapCount) / 2;
+    
+    const startYPosition = ENEMY_SPRITE_SIZE;
+
+    for (let y = 0; y < enemyLayout.length; y++) {
+      for (let x = 0; x < enemyLayout[y].length; x++) {
+        if (enemyLayout[y][x] === 'x') {
+          const spriteX = x * ENEMY_SPRITE_SIZE;
+          const spriteY = y * ENEMY_SPRITE_SIZE;
+          const spreadX = x * (ENEMY_SPRITE_SIZE + gapSize)
+          const spreadY = y * (ENEMY_SPRITE_SIZE + gapSize);
+          
+          const basePosition = new Vector2(spriteX, spriteY + startYPosition)
+          this.baseFormationPositions.push(basePosition);
+          this.destFormationPositions.push(basePosition.add(new Vector2(FORMATION_HORIZONTAL_MOVEMENT, 0)))
+          this.spreadFormationPositions.push(new Vector2(
+            spreadPositionsStartX + spreadX,
+            startYPosition + spreadY
+          ));
+        }
+      }
+    }
+  }
+  
+  /** 
+   * When called, this will set the new destination formation positions to be the spread
+   * formation and the current formation at the time of the call will be set to the base formation
+   * position.
+   */
+  #switchToSpreadFormation() {
+    const gameManager = GameManager.getInstance();
+    for (let entityId of this.enemies) {
+      /** @type {Enemy} */
+      const enemy = gameManager.entities.get(entityId);
+      if (!enemy) {
+        continue;
+      }
+      const newBasePosition = enemy.formationPosition;
+      this.baseFormationPositions.set(entityId, newBasePosition);
+    }
+    
+    this.destFormationPositions = this.spreadFormationPositions;
+    this.elapsedMovementTime = 0;
+    this.formationMovementTime = FORMATION_MOVEMENT_TIME;
+  }
+  
+  /**
+   * Initiates the transition to the center formation.
+   */
+  transitionToCenterFormation() {
+    const movementCyclePercentage = this.elapsedMovementTime / this.formationMovementTime;
+    this.formationMovementTime /= 2;
+    const gameManager = GameManager.getInstance();
+    
+    /** @type {Map<number, Vector2>} */
+    const centerPositions = new Map();
+    for (let entityId of this.enemies) {
+      /** @type {Enemy} */
+      const enemy = gameManager.entities.get(entityId);
+      if (!enemy) {
+        continue;
+      }
+      const centerPosition = lerp(this.baseFormationPositions.get(entityId), this.destFormationPositions.get(entityId), 0.5);
+      if (movementCyclePercentage === 0.5) {
+        // Snap to center position since we are there (give or take a little floating point error)
+        enemy.formationPosition = centerPosition;
+      } else {
+        centerPositions.set(entityId, centerPosition);
+      }
+    }
+
+    if (movementCyclePercentage > 0.5) {
+      this.elapsedMovementTime -= this.formationMovementTime;
+      this.baseFormationPositions = centerPositions;
+      this.cyclesUntilFormationSwitch = 1;
+    } else if (movementCyclePercentage < 0.5) {
+      this.destFormationPositions = centerPositions;
+      this.cyclesUntilFormationSwitch = 0;
+    } else {
+      // Activate next formation layout since we don't need
+      // to wait for the enemies to move into the proper position.
+      // They are alraedy there due to snapping done above.
+      this.#switchToSpreadFormation();  
+    }
+  }
+
+  /** @type {number} */
+  update(elapsedTime) {
+    const gameManager = GameManager.getInstance();
+    
+    // Reverse the direction of the formation every FORMATION_MOVEMENT_TIME
+    this.elapsedMovementTime += elapsedTime;
+    if (this.elapsedMovementTime >= this.formationMovementTime) {
+      if (this.cyclesUntilFormationSwitch === 0) {
+        this.#switchToSpreadFormation();
+        this.cyclesUntilFormationSwitch = Infinity;
+      } else {
+        this.elapsedMovementTime -= this.formationMovementTime;
+        const temp = this.baseFormationPositions;
+        this.baseFormationPositions = this.destFormationPositions;
+        this.destFormationPositions = temp;
+        this.cyclesUntilFormationSwitch--;
+      }
+    }
+
+    for (let entityId of this.enemies) {
+      /** @type {Enemy} */
+      const enemy = gameManager.entities.get(entityId);
+      if (!enemy) {
+        continue;
+      }
+      
+      const basePosition = this.baseFormationPositions.get(enemy.id);
+      const destPosition = this.destFormationPositions.get(enemy.id);
+      enemy.formationPosition = lerp(basePosition, destPosition, this.elapsedMovementTime / this.formationMovementTime);
+    }
+  }
+}
+
 export class EnemyManager {
   constructor() {
     /** @type {Set<number>} */
